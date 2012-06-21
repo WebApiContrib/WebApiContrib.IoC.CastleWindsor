b66a7f65711c56a670f2223f483820fb63487a92
Ryan Riley <ryan.riley@panesofglass.org>
2012-06-21 09:19:32 -0500

    Fix tabs/spaces.

--- src/WebApiContrib.IoC.CastleWindsor/WindsorResolver.cs
+++ src/WebApiContrib.IoC.CastleWindsor/WindsorResolver.cs
@@ -6,8 +6,8 @@ using Castle.Windsor;
 
 namespace WebApiContrib.IoC.CastleWindsor
 {
-	public class WindsorDependencyScope : IDependencyScope
-	{
+    public class WindsorDependencyScope : IDependencyScope
+    {
         private IWindsorContainer container;
 
         public WindsorDependencyScope(IWindsorContainer container)
@@ -18,8 +18,8 @@ namespace WebApiContrib.IoC.CastleWindsor
 
         public object GetService(Type serviceType)
         {
-			if (container == null)
-				throw new ObjectDisposedException("this", "This scope has already been disposed.");
+            if (container == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed.");
 
             if (!container.Kernel.HasComponent(serviceType))
                 return null;
@@ -29,8 +29,8 @@ namespace WebApiContrib.IoC.CastleWindsor
 
         public IEnumerable<object> GetServices(Type serviceType)
         {
-			if (container == null)
-				throw new ObjectDisposedException("this", "This scope has already been disposed.");
+            if (container == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed.");
 
             if (!container.Kernel.HasComponent(serviceType))
                 return Enumerable.Empty<object>();
@@ -38,31 +38,31 @@ namespace WebApiContrib.IoC.CastleWindsor
             return container.ResolveAll(serviceType).Cast<object>();
         }
 
-		public void Dispose()
-		{
-			container.Dispose();
-			container = null;
-		}
-	}
+        public void Dispose()
+        {
+            container.Dispose();
+            container = null;
+        }
+    }
 
     public class WindsorResolver : WindsorDependencyScope, IDependencyResolver
     {
         private readonly IWindsorContainer container;
 
         public WindsorResolver(IWindsorContainer container)
-			: base(container)
+            : base(container)
         {
             if (container == null)
-				throw new ArgumentNullException("container");
+                throw new ArgumentNullException("container");
 
             this.container = container;
         }
 
-    	public IDependencyScope BeginScope()
-    	{
-    		var scope = new WindsorContainer();
-			container.AddChildContainer(container);
-			return new WindsorDependencyScope(scope);
-    	}
+        public IDependencyScope BeginScope()
+        {
+            var scope = new WindsorContainer();
+            container.AddChildContainer(container);
+            return new WindsorDependencyScope(scope);
+        }
     }
 }
